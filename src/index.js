228384b0605271120d0871760267398bfbc108cb
gnmkrtchyan <gnmkrtchyan@gmail.com>
2018-11-13 23:21:31 +0300

    task3

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import readlineSync from 'readline-sync';
 const sayHello = () => console.log('Welcome to the Brain Games!');
 
 const askName = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
 
 export {
-    sayHello,
-    askName
-}
+  sayHello,
+  askName,
+};
