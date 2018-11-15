f19f9d72624ed2d87e00cf75cf81e1fc3c81bee0
gnmkrtchyan <gnmkrtchyan@gmail.com>
2018-11-15 22:56:49 +0300

    fixed task4

--- src/game.js
+++ src/game.js
@@ -1,10 +1,12 @@
 import readlineSync from 'readline-sync';
-import { head, tail } from './utils';
+import { car, cdr } from 'hexlet-pairs';
+
+const steps = 3;
 
 const playRound = (generateFn) => {
   const gameData = generateFn();
-  const question = head(gameData);
-  const correctAnswer = tail(gameData);
+  const question = car(gameData);
+  const correctAnswer = cdr(gameData);
 
   console.log(`Question: ${question}`);
   const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
@@ -17,20 +19,18 @@ const playRound = (generateFn) => {
   return isCorrect;
 };
 
-const playGame = (steps, gameDescription, generateFn) => {
+const playGame = (gameDescription, generateFn) => {
   console.log('\nWelcome to the Brain Games!');
   console.log(gameDescription);
 
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 
-  let n = 0;
-  while (n < steps) {
+  for (let i = 0; i < steps; i += 1) {
     if (!playRound(generateFn)) {
       console.log(`Let's try again, ${userName} !\n`);
       return;
     }
-    n += 1;
   }
   console.log(`Congratulations, ${userName} !\n`);
 };
