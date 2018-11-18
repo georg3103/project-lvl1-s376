dfd2b07fb6bbaefea4b8efd64a50261c8fff7295
gnmkrtchyan <gnmkrtchyan@gmail.com>
2018-11-18 13:38:41 +0300

    fixed task8

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,18 +3,21 @@ import getRandomNum from '../utils';
 import playGame from '../game';
 
 const isPrime = (num) => {
-  for (let i = 2; i < num; i += 1) {
-    if (num % i === 0) {
-      return false;
+  if (num > 0) {
+    for (let i = 2; i < num; i += 1) {
+      if (num % i === 0) {
+        return false;
+      }
     }
+    return num !== 1 && num !== 0;
   }
-  return num !== 1 && num !== 0;
+  return false;
 };
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const generateGame = () => {
-  const question = getRandomNum(1, 100);
+  const question = getRandomNum(-100, 100);
   const answer = isPrime(question) ? 'yes' : 'no';
 
   return cons(question, String(answer));
